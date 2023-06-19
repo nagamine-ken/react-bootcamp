a56038f7ef53c17b8cc5019c637270d52c1e55d6
Ken Nagamine <nagamine.digitalholding@gmail.com>
2023-06-19 12:18:36 -0700

    Keeper App part 2

--- src/App.js
+++ src/App.js
@@ -1,63 +1,54 @@
 import "./App.css";
-// import Header from "./header.jsx";
-// import Footer from "./Footer.jsx";
-// import Note from "./Note.jsx";
+import Header from "./header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
 // import contacts from "./contacts";
-import Entry from "./Entry";
-import emojipedia from "./emojipedia";
-
-
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//       {contacts.map((contact)=>{
-//         return (
-//           <Note
-//             // All of these data is passed to the Note.jsx child as an object called "props":
-//             id={contact.id}
-//             key={contact.id}
-//             name={contact.name}
-//             imgURL={contact.imgURL}
-//             phone={contact.phone}
-//             email={contact.email}
-//           />
-//         );
-//       })}
-      
-//       <Footer />
-//     </div>
-//   );
-// }
-
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      // All of these data is passed to the Entry.jsx child as an object called "props":
-      //When mapping components you have to assign an unique "key" to each mapped component!!!
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      meaning={emojiTerm.meaning}
-    />
-  );
-}
+import notes from "./notes";
+// import emojipedia from "./emojipedia";
 
 function App() {
   return (
     <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
+      <Header />
+      {notes.map((notes)=>{
+        return (
+          <Note
+            // All of these data is passed to the Note.jsx child as an object called "props":
+            key={notes.id}
+            title={notes.title}
+            content={notes.content}
+          />
+        );
+      })}
 
-      <dl className="dictionary">
-
-          { emojipedia.map(createEntry) }
-
-      </dl>
+      <Footer />
     </div>
   );
 }
 
+// function App() {
+//   return (
+//     <div>
+//       <h1>
+//         <span>emojipedia</span>
+//       </h1>
+
+//       <dl className="dictionary">
+//         {emojipedia.map( (emojiTerm) => {
+//           return (
+//             <Entry
+//               // All of these data is passed to the Entry.jsx child as an object called "props":
+//               //When mapping components you have to assign an unique "key" to each mapped component!!!
+//               key={emojiTerm.id}
+//               emoji={emojiTerm.emoji}
+//               name={emojiTerm.name}
+//               meaning={emojiTerm.meaning}
+//             />
+//           );
+//         })}
+//       </dl>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,23 +1,14 @@
 import "./App.css";
-import Avatar from "./Avatar";
+// import Avatar from "./Avatar";
 import Detail from "./Detail";
 
 function Note(props) {
   return (
     <div>
-      {/* <h1 className="heading">My Contacts</h1> */}
-
-      <div className="card">
-        <div className="top">
-          <h2 className="name">{props.name}</h2>
+      <div className="note">
           {/* Here we get the data from the parent App.js as a "props" property, and pass it to the Avatar.jsx child as follows: */}
-          <Avatar imgURL={props.imgURL} />
-        </div>
-        <div className="bottom">
-          <Detail detailInfo={props.id} />
-          <Detail detailInfo={props.phone} />
-          <Detail detailInfo={props.email} />
-        </div>
+          <h1>{props.title}</h1>
+          <Detail detailInfo={props.content} />
       </div>
     </div>
   );
